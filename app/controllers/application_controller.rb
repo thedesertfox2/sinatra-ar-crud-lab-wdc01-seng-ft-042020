01222c2d37f8a67a2d36b480540dc436d46b7ee6
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-05-12 17:02:16 -0400

    with injae

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+
+  get "/" do
+    erb :homepage
   end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  post "/articles" do
+    title = params[:title]
+    content = params[:content]
+    article = Article.find_or_create_by(
+      title: title,
+      content: content
+      )
+    redirect "/articles/#{article.id}"
+
+  end
+
+  get "/articles/:id" do
+    article_id = params[:id]
+    @article = Article.find(article_id)
+    erb :show
+  end
+
 end
+
+
+
